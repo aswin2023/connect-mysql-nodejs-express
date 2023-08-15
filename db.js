da2c025cb64ed228abb736014ecd397620499a36
Ashwani <vcsashwani@gmail.com>
2023-08-15 12:36:47 +0530

    conect mysql

--- db.js
+++ db.js
@@ -1,1 +1,14 @@
-console.log("hhhhhhhhhh")
+const mysql = require("mysql2");
+
+// Create a pool of database connections
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Password@123",
+  database: "admin_motionlab",
+  waitForConnections: true,
+  connectionLimit: 10, // Adjust this value based on your needs
+  queueLimit: 0,
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db = require("./db"); // Import the db.js module
 
 // Define a route
 app.get("/", (req, res) => {
-  res.send("Server is working");
+  //   res.send("Server is working");
+  // Attempt to get a connection from the pool
+  db.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({ error: "Database connection error" });
+    }
+
+    // Release the connection
+    connection.release();
+
+    // If no error occurred, the database is available
+    res.json({ message: "Database is available" });
+  });
 });
 
 // listner
